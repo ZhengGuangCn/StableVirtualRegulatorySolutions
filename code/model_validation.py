36470baa6c441a8d39739a32840285c7197c212a
ZhengGuangCn <forzhengguang@163.com>
2025-01-18 11:23:21 +0800

    Update model_validation.py

--- code/model_validation.py
+++ code/model_validation.py
@@ -1,22 +1,23 @@
 """
-    The purpose of the following programs is to use the final model solution to validate the model's performance in distinguishing between healthy and disease samples.
+The purpose of the following programs is to use the final model solution to validate the model's performance in distinguishing between healthy and disease samples.
 
-    In order to make it easier for us to visualise it later, we filtered out those connections with a weight of 0 when we saved the parameters of the model during the training phase.
-    Note: Since we are using a fully connected network, but the actual regulatory network is sparse, the weights of those connections that do not actually exist in the network are forced to 0 in the training phase.
-    Now we want to reassign the parameters of the final solution to the network, we take the following approach:
-    load one of the models saved in the training phase as our validation network and read its model We do this by loading a model saved in the training phase as our validation network, reading its model parameters,
-    and overriding the non-zero weights of the model with the parameters of the final solution, thus replacing them one by one, with the caveat that all the connections have non-zero weights except for the ones that we forced to be 0. After doing this,
-    all the parameters of the network are now updated to the parameters of the final solution.
+In order to make it easier for us to visualise it later, we filtered out those connections with a weight of 0 when we saved the parameters of the model during the training phase.
+Note: Since we are using a fully connected network, but the actual regulatory network is sparse, the weights of those connections that do not actually exist in the network are forced to 0 in the training phase.
+Now we want to reassign the parameters of the final solution to the network, we take the following approach:
+load one of the models saved in the training phase as our validation network and read its model We do this by loading a model saved in the training phase as our validation network, reading its model parameters,
+and overriding the non-zero weights of the model with the parameters of the final solution, thus replacing them one by one, with the caveat that all the connections have non-zero weights except for the ones that we forced to be 0. After doing this,
+all the parameters of the network are now updated to the parameters of the final solution.
 
-    Finally, the input values of each real disease sample and healthy sample are fed into the network to obtain the output values of each layer.
-    After flattening the network's output values, the Euclidean distance between these output values and the corresponding real sample's node values at each layer is calculated.
-    A smaller distance indicates that the sample aligns more closely with the disease pattern described by the model solution.
-    For example, if we use the model solution for disease samples, a smaller distance for a real sample after input suggests a higher probability that the real sample is a disease sample.
+Finally, the input values of each real disease sample and healthy sample are fed into the network to obtain the output values of each layer.
+After flattening the network's output values, the Euclidean distance between these output values and the corresponding real sample's node values at each layer is calculated.
+A smaller distance indicates that the sample aligns more closely with the disease pattern described by the model solution.
+For example, if we use the model solution for disease samples, a smaller distance for a real sample after input suggests a higher probability that the real sample is a disease sample.
 
-    model_path: The reference model is used to provide the positions of non-zero weights in the network. We replace the parameters of this reference model one by one with the parameters from the final model solution.
-    weight: The weights of all connections in the final model solution are all non-zero.
-    bias: The biases of each node in the final model solution.
-    input_file: The values of each real sample, including input node values and values of nodes in each layer, are used as follows:
+File Introduction:
+model_path: The reference model is used to provide the positions of non-zero weights in the network. We replace the parameters of this reference model one by one with the parameters from the final model solution.
+weight: The weights of all connections in the final model solution are all non-zero.
+bias: The biases of each node in the final model solution.
+input_file: The values of each real sample, including input node values and values of nodes in each layer, are used as follows:
             the input node values are fed into the model to obtain the predicted values of nodes in each layer, while the values of nodes in each layer are used to validate the model's predictive performance.
 """
 
@@ -44,7 +45,7 @@ class NeuralNetwork(nn.Module):
         intermediate_outputs.append(x)
         return x, intermediate_outputs
 
-model_path = "T_RA16_0.pth"
+model_path = "../data/T_RA16_0.pth"
 original_model = torch.load(model_path)
 
 # Extracting weights and biases from the original model
@@ -56,8 +57,8 @@ for layer in original_model.layers:
         reference_biases.append(layer.bias.data.clone())     # clone bias
 
 # Loading new non-zero weights and bias data
-weight = pd.read_csv('mean_a_closest_100_1_virtual-T-RA16.csv')
-bias = pd.read_csv('mean_b_closest_100_1_virtual-T-RA16.csv')
+weight = pd.read_csv('../data/mean_a_closest_100_1_virtual-T-RA16.csv')
+bias = pd.read_csv('../data/mean_b_closest_100_1_virtual-T-RA16.csv')
 
 weight = weight.apply(pd.to_numeric, errors='coerce').iloc[:, 0].to_list()
 print(len(weight))
@@ -68,7 +69,7 @@ print(bias)
 
 # Create a new model with the same structure as the original model
 num_nodes_per_layer = [layer.weight.size(1) for layer in original_model.layers if isinstance(layer, nn.Linear)]
-num_nodes_per_layer.append(original_model.layers[-1].weight.size(0))  # 最后一层输出维度
+num_nodes_per_layer.append(original_model.layers[-1].weight.size(0))
 net = NeuralNetwork(num_nodes_per_layer)
 
 # Fill in the new weight matrix, leaving the zero position of the reference model unchanged
@@ -113,7 +114,7 @@ for i, layer in enumerate(net.layers):
 
 # Testing the effectiveness of model validation
 for i in range(1, 8):
-    input_file = pd.read_excel('T-real_samples.xlsx')
+    input_file = pd.read_excel('../data/T-real_samples.xlsx')
     input_data = pd.to_numeric(input_file.iloc[:12, i], errors='coerce').values
     input_data = torch.tensor(input_data, dtype=torch.float32)
     # print("input_data:", input_data)
