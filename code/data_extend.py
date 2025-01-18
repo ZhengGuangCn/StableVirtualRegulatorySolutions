33fa1d897da0c3f975b039e25910cf73ea8bca90
ZhengGuangCn <forzhengguang@163.com>
2025-01-18 08:32:47 +0800

    Update data_extend.py

--- code/data_extend.py
+++ code/data_extend.py
@@ -6,7 +6,7 @@
 import pandas as pd
 import numpy as np
 
-data = pd.read_excel('T-RA16.xlsx')
+data = pd.read_excel('../data/T-RA16.xlsx')
 # Input layer data and output layer data for real samples.
 data = pd.DataFrame(data)
 data = data.T
